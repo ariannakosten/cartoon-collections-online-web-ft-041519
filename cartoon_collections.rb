7c30d47f9f0c2a5d2473a48ae6a891a049c01155
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-28 23:09:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 require 'pry'
 
+=======
+>>>>>>> 6bd1c7982a2b9caeaf7d6a2de7eb7df94a24448c
 def roll_call_dwarves(array)
  array.each_with_index do |name, index|
     i = index + 1
@@ -15,9 +18,22 @@ def long_planeteer_calls(calls_array)
   calls_array.any? {|word| word.length > 4}
 end
 
+<<<<<<< HEAD
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
     array.find do |word| 
       cheese_types.include?(word)
   end
 end
+=======
+def find_the_cheese(strings)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+    strings.find do |cheese| 
+      if strings.include?(cheese_types)
+    puts cheese.to_s
+  else 
+    puts nil
+    end
+  end
+end
+>>>>>>> 6bd1c7982a2b9caeaf7d6a2de7eb7df94a24448c
